899b9f617ae97736bd3ca3d61d803cce4c8de62a
拂尘敲木鱼 <550947464@qq.com>
2022-08-10 00:02:28 +0800

    [Add] 提交Lua数组实现，以及补充红黑树删除概念

--- 数据结构与算法/2.数组/lua/array.lua
+++ 数据结构与算法/2.数组/lua/array.lua
@@ -1,1 +1,53 @@
-print("Hello,world")
+ArrayList = {
+  mCap = 0,
+  mData = {},
+  mLength = 0,
+}
+
+function ArrayList:New(capacity)
+  self.mCap = capacity
+  self.mData = {}
+  self.mLength = 0
+end
+
+function ArrayList:Insert(index, data)
+  assert(type(index) == "int", "index is not a int")
+  assert(self.mCap == self.mData, "数组没有多余空间进行添加")
+  assert(index < 0 and index >= self.mCap, "数组下标超出数组范围")
+
+  for i = self.mLength-1, self.mLength-index, -1 do
+    self.mData[i] = self.mData[i-1]
+  end
+
+  self.mData[index] = data
+  self.mLength = self.mLength + 1
+
+  return true
+end
+
+function ArrayList:Find(index)
+  assert(type(index) == "int", "index is not a int")
+  assert(index < 0 and index >= self.mCap, "数组下标超出数组范围")
+
+  result["result"] = true
+  result["data"] = self.mData[index]
+
+  return result
+end
+
+function ArrayList:Delete(index)
+  assert(type(index) == "int", "index is not a int")
+  assert(index < 0 and index >= self.mLength, "数组下标超出数组范围")
+
+  data = self.mData[index]
+  while(self.mData[index] ~= nil)
+  do
+    self.mData[index] = self[index+1]
+    index = index+1
+  end
+
+  result["result"] = true
+  result["data"] = data
+
+  return result
+end
