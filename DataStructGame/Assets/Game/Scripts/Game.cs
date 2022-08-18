5b58bca809a6884a2ccc739096e53d8ee46cd483
zhangjingjia1 <zhangjingjia1@kingsoft.com>
2022-08-18 20:40:33 +0800

    【add】补充遗漏代码

--- DataStructGame/Assets/Game/Scripts/Game.cs
+++ DataStructGame/Assets/Game/Scripts/Game.cs
@@ -11,32 +11,42 @@ namespace Game.Scripts {
     public class Game : MonoBehaviour {
         // Start is called before the first frame update
         DoubleLinkedList<int> double_list = new DoubleLinkedList<int>();
-        //ChachePool<int> chachePool = new ChachePool<int>();
+
+        private Array<DoubleLinkedNode<int>> array = new Array<DoubleLinkedNode<int>>();
+        CachePool<int> cachePool = new CachePool<int>();
         
         void Start() {
-            var i = 0;
-            while (i < 500) {
-                double_list.Append(i+i);
-                i++;
-            }
-
-            var j = 0;
-            while (j < 200) {
-                var deleteNode = double_list.DeleteFirstNode();
-                //hachePool.Put(deleteNode);
-                j++;
-            }
-
-            double_list.PrintAll();
-            
-            Debug.Log("*******************************************************");
-            
-            double_list.CachePool.PrintAll();
+            // var i = 0;
+            // while (i < 500) {
+            //     double_list.Append(i+i);
+            //     i++;
+            // }
+            //
+            // var j = 0;
+            // while (j < 500) {
+            //     double_list.DeleteFirstNode();
+            //     j++;
+            // }
         }
 
         // Update is called once per frame
         void Update() {
+            var i = 0;
+            while (i < 500) {
+                double_list.Append(i+i);//add
+                //var a = double_list.Last;
+                //array.Insert(array.length, double_list.Last);
+                //double_list.DeleteFirstNode();
+                //cachePool.Put(double_list.Last);
+                i++;
+            }
             
+            // var j = 0;
+            // while (j < 50) {
+            //     var a = double_list.DeleteFirstNode();
+            //     j++;
+            // }
+            //double_list.Clear();
         }
         
     }   
